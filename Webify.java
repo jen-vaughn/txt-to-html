4965c36034916083971a3506e70cfc33852f4fa6
jen-vaughn <93220451+jen-vaughn@users.noreply.github.com>
2022-11-13 12:06:40 -0500

    Update Webify.java

--- Webify.java
+++ Webify.java
@@ -2,33 +2,31 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Webify {
-	public static void main(String[] args) throws FileNotFoundException, IOException {		//ioexcpetion needed??
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// Jennifer Vaughn
-		// CIS 1068, Section 1
-		// Assignment 5: html Converter
+		// 10/5/2022
+		// .txt to .html
 		// This program will convert a given .txt file to a .html file.
-		// 10/5/22
 		
 		Scanner CONSOLE = new Scanner(System.in);
 		
-		//1. Scan console for file name
+		//Scans console for file name
 		System.out.println("Please input the .txt file name: ");
 		String fileName = CONSOLE.nextLine();
 		
-		//trims the file name
+		//Trims the file name
 		String fileNameWOSuffix = fileName.substring(0, fileName.length()-4);
 		//creates and prints the HTML file name
 		String htmlFileName = fileNameWOSuffix + ".html";
 		System.out.println(htmlFileName);
 	
-		//Scan the input file
+		//Scans the input file
 		Scanner input = new Scanner(new File(fileName));	//input file
 		PrintStream output = new PrintStream(new File(htmlFileName));	//output file
 		
 		
 				
-		//2 and 3. Find rule we should follow and add HTML tag
-		//print beginning of HTML file
+		//Print beginning of HTML file
 		output.println("<html>");
 		output.println("<body>");
 		
@@ -77,7 +75,7 @@ public class Webify {
 		
 		while (lineScan.hasNext())	{
 			String word = lineScan.next();	//get each word of line
-			//if one word starts with [[ and ends with ]], it is URL
+			//if one word starts with [[ and ends with ]], then it is a URL
 			if (word.startsWith("[[") && word.endsWith("]]"))	{
 				//remove the [[ and ]]
 				String noBrackets = word.substring(2, word.length()-2);
